c28ce71f33b65f6c75b83252070c1acf20ee288b
hh <hhio618@gmail.com>
2021-11-14 21:09:07 +0330

    add PoHEvidence contract

--- contracts/Evidence.sol
+++ contracts/Evidence.sol
@@ -4,59 +4,46 @@
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.7.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 
 // This is the main building block for smart contracts.
-contract Token {
-    // Some string type variables to identify the token.
-    // The `public` modifier makes a variable readable from outside the contract.
-    string public name = "My Hardhat Token";
-    string public symbol = "MHT";
+contract PoHEvidence is Ownable {
 
-    // The fixed amount of tokens stored in an unsigned integer type variable.
-    uint256 public totalSupply = 1000000;
+    event Delegated(address owner, address delegatee);
 
-    // An address type variable is used to store ethereum accounts.
-    address public owner;
+    // @notice records of all delegatees
+    mapping (address => bool) internal _delegatees;
 
-    // A mapping is a key/value map. Here we store each account balance.
-    mapping(address => uint256) balances;
+    // @notice A Evidence for recording the PoH evidence on-chain
+    struct Evidence {
+        address user;
+        address delegatee;
+        uint timestamp;
+        bytes32 evidence;
+    }
+    
+    uint32 numEvidences;
+    mapping(address => Evidence) public evidences;
 
-    /**
-     * Contract initialization.
-     *
-     * The `constructor` is executed only once when the contract is created.
-     */
-    constructor() {
-        // The totalSupply is assigned to transaction sender, which is the account
-        // that is deploying the contract.
-        balances[msg.sender] = totalSupply;
-        owner = msg.sender;
+    
+    function addEvidenceDelegate(address newEvidenceDelegate) external onlyOwner {
+        require(!_delegatees[newEvidenceDelegate], "delegatee already exists");
+        _delegatees[newEvidenceDelegate] = true;
+        emit Delegated(msg.sender, newEvidenceDelegate);
     }
 
-    /**
-     * A function to transfer tokens.
-     *
-     * The `external` modifier makes a function *only* callable from outside
-     * the contract.
-     */
-    function transfer(address to, uint256 amount) external {
-        // Check if the transaction sender has enough tokens.
-        // If `require`'s first argument evaluates to `false` then the
-        // transaction will revert.
-        require(balances[msg.sender] >= amount, "Not enough tokens");
-
-        // Transfer the amount.
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
+    function delegateeExists(address delegatee)  external view onlyOwner returns(bool exists){
+        return _delegatees[delegatee];
     }
 
-    /**
-     * Read only function to retrieve the token balance of a given account.
-     *
-     * The `view` modifier indicates that it doesn't modify the contract's
-     * state, which allows us to call it without executing a transaction.
-     */
-    function balanceOf(address account) external view returns (uint256) {
-        return balances[account];
+    function addEvidence(address user, address delegatee, uint timestamp, bytes32 evidence) external onlyOwner{
+        Evidence memory ev = evidences[user];
+        require(ev.user == address(0), "an evidence already exists for this user");
+        ev.user = user;
+        ev.delegatee = delegatee;
+        ev.timestamp = timestamp;
+        ev.evidence = evidence;
+        numEvidences ++;
     }
 }
