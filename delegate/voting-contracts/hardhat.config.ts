c34e6df5bc0df48c23cec623e6f9143e31b7d5eb
seyedrezafar <seyedrezafar1093@gmail.com>
2021-11-21 22:03:50 +0330

    add verify page, add test base funtionality

---
+++ delegate/voting-contracts/hardhat.config.ts
@@ -0,0 +1,144 @@
+// hardhat.config.ts
+
+import "dotenv/config"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-solhint"
+import "@tenderly/hardhat-tenderly"
+import "@nomiclabs/hardhat-waffle"
+import "hardhat-abi-exporter"
+import "hardhat-deploy"
+import "hardhat-deploy-ethers"
+import "hardhat-gas-reporter"
+import "hardhat-spdx-license-identifier"
+import "hardhat-typechain"
+import "@nomiclabs/hardhat-web3";
+import "hardhat-watcher"
+import "solidity-coverage"
+
+import { HardhatUserConfig } from "hardhat/types"
+import { removeConsoleLog } from "hardhat-preprocessor"
+import { web3, Web3 } from "hardhat"
+
+
+const accounts = {
+  mnemonic: process.env.MNEMONIC || "test test test test test test test test test test test junk",
+  // accountsBalance: "990000000000000000000",
+}
+
+const config: HardhatUserConfig = {
+  abiExporter: {
+    path: "./abi",
+    clear: false,
+    flat: true,
+    // only: [],
+    // except: []
+  },
+  defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    currency: "USD",
+    enabled: process.env.REPORT_GAS === "true",
+    excludeContracts: ["contracts/mocks/", "contracts/libraries/"],
+  },
+  mocha: {
+    timeout: 20000,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    dev: {
+      // Default to 1
+      default: 1,
+      // dev address mainnet
+      // 1: "",
+    },
+  },
+  networks: {
+    localhost: {
+      live: false,
+      saveDeployments: true,
+      tags: ["local"],
+    },
+    hardhat: {
+      forking: {
+        enabled: process.env.FORKING === "true",
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      },
+      live: false,
+      saveDeployments: true,
+      tags: ["test", "local"],
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.MNEMONIC}`],
+      gasPrice: 50000000000,
+    },
+  },
+  paths: {
+    artifacts: "artifacts",
+    cache: "cache",
+    deploy: "deploy",
+    deployments: "deployments",
+    imports: "imports",
+    sources: "contracts",
+    tests: "test",
+  },
+  preprocess: {
+    eachLine: removeConsoleLog((bre) => bre.network.name !== "hardhat" && bre.network.name !== "localhost"),
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.7.3",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.5.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.4.22",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  spdxLicenseIdentifier: {
+    overwrite: false,
+    runOnCompile: true,
+  },
+  tenderly: {
+    project: process.env.TENDERLY_PROJECT!,
+    username: process.env.TENDERLY_USERNAME!,
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v5",
+  },
+  watcher: {
+    compile: {
+      files: ["./contracts"],
+      verbose: true,
+    },
+  },
+}
+
+export default config
---
+++ delegate/voting-contracts/scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    const Evidence = await ethers.getContractFactory("CoreEvidence");
+    const evidence = await Evidence.deploy();
+  
+    console.log("PoH-Evidence address:", evidence.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
